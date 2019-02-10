a6c011a98cf9950657809c8399f2665630f4c2a1
Dmitry Kostyrev <xtruthonlyx@gmail.com>
2019-02-10 14:40:04 +0300

    Move LoginActivity to ui package

--- app/src/main/java/com/joom/yetanotherapp/ui/LoginActivity.kt
+++ app/src/main/java/com/joom/yetanotherapp/ui/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.joom.yetanotherapp
+package com.joom.yetanotherapp.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.joom.yetanotherapp.R
 
 class LoginActivity : AppCompatActivity() {
 
